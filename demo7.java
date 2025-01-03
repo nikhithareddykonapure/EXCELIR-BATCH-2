871b170e89578bdea470c63e06ca90db06203a9c
nikhithareddykonapure <nikhithareddyk48@gmail.com>
2025-01-03 21:15:50 +0530

    Add files via upload

---
+++ demo7.java
@@ -0,0 +1,11 @@
+public class demo7 {
+    public static void main(String[]args) {
+        String name = "nikhitha";
+        String password = "123456789";
+        while((name == "nikhitha") && (password == "123456789")) {
+            System.out.println("this is nikhitha");
+            break;
+        }
+    }
+    
+}
